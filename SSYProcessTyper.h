37c125dd19172e3bc85e0d742e899269ef5bf47c
Jerry Krinock <jerry@sheepsystems.com>
2013-11-05 16:16:35 -0800

    Finished fixing SSYProcessTyper to TransformProcessType() compatible with 10.9 Mavericks

--- SSYProcessTyper.h
+++ SSYProcessTyper.h
@@ -26,11 +26,48 @@ typedef enum SSYProcessTyperType_enum SSYProcessTyperType ;
 
 + (SSYProcessTyperType)currentType ;
 
+/*
+ @brief    Transforms the current process to foreground type if it is not.
+ 
+ @details  Starting with BookMacster 1.19.6, this method features a "dance
+ with Finder" kludge.  This kludge is to fix the issue which arose in Mac
+ OS X 10.9 Mavericks, after I began invoking this method in app delegate's
+ -applicationDidFinishLaunching instead of -init.  The new issue was that the
+ menu still would not show when launched from an app such as Alfred version 2,
+ which is itself an LSUIElement background app.  According to Alfred
+ developer Andrew Pepperrell, Alfred 2 uses a standard NSWorkspace method to
+ launch apps, nothing tricky.
+ http://www.alfredforum.com/topic/3358-application-bookmacster-menu-does-not-show-immediately/
+ The solution is to do what I call a "dance with Finder", and supposedly this
+ kludge was actually recommended by Apple
+ http://stackoverflow.com/questions/7596643/when-calling-transformprocesstype-the-app-menu-doesnt-show-up
+ But I modified it somewhat.  First of all, I found that the delays of 0.1 did
+ not help, and further that if the app was not launched by Finder but by some
+ other means (Alfred, LaunchBar, AppleScript, open(1), etc.) then the Finder's
+ menu would flash before BookMacster's menu would show.  Therefore I added
+ additional code to detect when showing the menu failed and only initiate the
+ "dance with Finder" if it fails, which should be only if launched by Alfred 2.
+ Fortunately, for 10.7 and later users, method
+ -[NSWorkspace sharedWorkspace] menuBarOwningApplication]
+ provides the detection.  The "dance with Finder" code is a real mess, but I
+ tested in 10.6, 10.7, 10.8, 10.9 and it works in all cases.
+ */
 + (void)transformToForeground:(id)sender ;
 
 #if (MAC_OS_X_VERSION_MAX_ALLOWED > 1060)
+
+/*
+ @brief    Transforms the current process to UIElement type if it is not.
+ @details  Available in Mac OS X 10.7 or later
+*/
 + (void)transformToUIElement:(id)sender ;
+
+/*
+ @brief    Transforms the current process to background type if it is not.
+ @details  Available in Mac OS X 10.7 or later
+ */
 + (void)transformToBackground:(id)sender ;
+
 #endif
 
 @end
--- SSYProcessTyper.m
+++ SSYProcessTyper.m
@@ -1,5 +1,8 @@
 #import "SSYProcessTyper.h"
 
+NSString* constKeyTimeoutDate = @"timeoutDate" ;
+NSString* constKeyTimeoutSelectorName = @"timeoutSelectorName"  ;
+
 @implementation SSYProcessTyper
 
 /*
@@ -72,10 +75,18 @@
     return activeAppPid ;
 }
 
+#define TIMEOUT_BEFORE_TRYING_DANCE_WITH_FINDER 1.5
+/*
+ The above should be long enough that it does not do the dance when launched
+ from Finder, but short enough that it is not too annoying for Alfred users
+ who will require the dance.
+ */
+#define TIMEOUT_AFTER_DANCE 0.5
+#define WAIT_TO_TEST_AFTER_DANCE 1.0
+#define MENU_BAR_RETRY_INTERVAL 0.025
+#define TIMEOUT_FOR_DANCE_WITH_FINDER 3.0
+
 + (void)bringFrontPid:(pid_t)pid {
-	// Remember that any NSLogs early in the app results in two "Help" menus
-	//NSLog(@"ActiveApp: %@", [[[NSWorkspace sharedWorkspace] activeApplication] objectForKey:@"NSApplicationName"]) ;
-	sleep(2) ;
 	ProcessSerialNumber psn ;
 	OSStatus err ;
 	err = GetProcessForPID(pid, &psn) ;
@@ -83,6 +94,74 @@
         NSLog(@"Internal Error 915-9386 %ld", (long)err) ;
     }
 	SetFrontProcess(&psn);
+}
+
++ (void)danceWithFinder {
+    for (NSRunningApplication * app in [NSRunningApplication runningApplicationsWithBundleIdentifier:@"com.apple.finder"]) {
+        [app activateWithOptions:NSApplicationActivateIgnoringOtherApps];
+        break;
+    }
+    [self performSelector:@selector(danceWithFinderStep2) withObject:nil afterDelay:0.0];
+}
+
++ (void)danceWithFinderStep2 {
+    ProcessSerialNumber psn = { 0, kCurrentProcess };
+    (void) TransformProcessType(&psn, kProcessTransformToForegroundApplication);
+    
+    [self performSelector:@selector(danceWithFinderStep3) withObject:nil afterDelay:0.0];
+}
+
++ (void)danceWithFinderStep3 {
+    [[NSRunningApplication currentApplication] activateWithOptions:NSApplicationActivateIgnoringOtherApps];
+
+    NSDictionary* info = [NSDictionary dictionaryWithObjectsAndKeys:
+                          [NSDate dateWithTimeIntervalSinceNow:TIMEOUT_AFTER_DANCE], constKeyTimeoutDate,
+                          @"danceWithFinder", constKeyTimeoutSelectorName,
+                          nil] ;
+
+    [self performSelector:@selector(takeMenuBarWithInfo:)
+               withObject:info
+               afterDelay:MENU_BAR_RETRY_INTERVAL] ;
+}
+
++ (void)takeMenuBarWithInfo:(NSDictionary*)info {
+    NSDate* timeoutDate = [info objectForKey:constKeyTimeoutDate] ;
+    if ([[NSDate date] earlierDate:timeoutDate] == timeoutDate) {
+        // Timeout
+        SEL timeoutSelector = NSSelectorFromString([info objectForKey:constKeyTimeoutSelectorName]) ;
+        [self performSelector:timeoutSelector] ;
+    }
+    else {
+        NSInteger gotMenuBarState = [self weGotMenuBar] ;
+        if (gotMenuBarState == NSOffState) {
+            [self performSelector:@selector(takeMenuBarWithInfo:)
+                       withObject:info
+                       afterDelay:MENU_BAR_RETRY_INTERVAL] ;
+        }
+        else {
+            // Succeeded getting the menu bar.  Done.
+        }
+    }
+}
+
++ (NSInteger)weGotMenuBar {
+    NSInteger answer ;
+    if (
+        NSClassFromString(@"NSRunningApplication")
+        &&
+        [[NSWorkspace sharedWorkspace] respondsToSelector:@selector(menuBarOwningApplication)]
+        ) {
+        NSRunningApplication* menuBarApp = [[NSWorkspace sharedWorkspace] menuBarOwningApplication] ;
+        BOOL weGotMenuBar = ([menuBarApp isEqual:[NSRunningApplication currentApplication]]) ;
+        answer = weGotMenuBar ? NSOnState : NSOffState ;
+    }
+    else {
+        // Mac OS X 10.6 or earlier..  We cannot test whether we got menu bar.
+        // Stupid -[NSMenu isVisible] returns YES even when we are LSUIElement.
+        answer = NSMixedState ;  // "We don't know."
+    }
+    
+    return answer ;
 }
 
 + (void)transformToType:(SSYProcessTyperType)type {
@@ -116,6 +195,12 @@
     
     if (doShow) {
 		[NSApp activateIgnoringOtherApps:YES] ;
+
+        NSDictionary* info = [NSDictionary dictionaryWithObjectsAndKeys:
+                              [NSDate dateWithTimeIntervalSinceNow:TIMEOUT_BEFORE_TRYING_DANCE_WITH_FINDER], constKeyTimeoutDate,
+                              @"danceWithFinder", constKeyTimeoutSelectorName,
+                              nil] ;
+        [self takeMenuBarWithInfo:info] ;
     }
 }
 
