d27235daec6bb06b0734b94caa1e021f519f0ae3
Jerry Krinock <jerry@sheepsystems.com>
2014-08-18 07:04:34 -0700

    Moved SSYLineBox files to proper location, arghhh.

---
+++ SSYLineBox.h
@@ -0,0 +1,18 @@
+#import <Cocoa/Cocoa.h>
+
+/*!
+ @brief    Subclass of NSBox which makes collapses into a grayscale vertical or
+ horizontal line, with whiteness=0.2, alpha=1.0, thickness=1.0 point, restoring
+ the darkness of the "Vertical Line" and "Horizontal Line" object that became
+ too white in OS X 10.10.
+ 
+ @details  This subclass is useful in OS X 10.10 Yosemite.  For some
+ reason, the "Vertical Line" and "Horizontal Line" NSBox objects in the
+ xib library, which produce a fairly dark line in earlier versions of OS X,
+ produce a very light gray line in OS X 10.10 that is barely discernable on
+ a white background.  Just change the object class of your Vertical Line and
+ Horizontal Line objects from NSBox to SSYLineBox
+ */
+@interface SSYLineBox : NSBox
+
+@end
---
+++ SSYLineBox.m
@@ -0,0 +1,39 @@
+#import "SSYLineBox.h"
+
+@implementation SSYLineBox
+
+#define DESIRED_WHITENESS 0.2
+#define DESIRED_ALPHA 1.0
+#define DESIRED_THICKNESS 1.0
+
+- (void)drawRect:(NSRect)dirtyRect {
+    NSColor* color = [NSColor colorWithCalibratedWhite:DESIRED_WHITENESS
+                                                 alpha:DESIRED_ALPHA] ;
+    [color setFill] ;
+
+    /* The thickness (frame.size.width of a Vertical Line, or frame.size.height
+     of a Horizontal Line) drawn out of the object library in Xcode's xib
+     editor is 5 points.  I don't know why, because the line itself is only 1
+     pixel thick.  Anyhow, the following code deals with that 5 (or whatever)
+     points, positioning the line drawn in the center of the frame. */
+    NSRect rect = NSZeroRect ;
+    if ([self frame].size.height > [self frame].size.width) {
+        // We want a vertical line
+        CGFloat frameThickness = [self frame].size.width ;
+        rect.origin.x = (frameThickness - DESIRED_THICKNESS)/2.0 ;
+        rect.size.width = DESIRED_THICKNESS ;
+        rect.size.height = [self frame].size.height ;
+    }
+    else {
+        // We want a horizontal line
+        CGFloat frameThickness = [self frame].size.height ;
+        rect.origin.y = (frameThickness - DESIRED_THICKNESS)/2.0 ;
+        rect.size.height = DESIRED_THICKNESS ;
+        rect.size.width = [self frame].size.width ;
+    }
+    
+    NSBezierPath *path = [NSBezierPath bezierPathWithRect:rect] ;
+    [path fill] ;
+}
+
+@end
