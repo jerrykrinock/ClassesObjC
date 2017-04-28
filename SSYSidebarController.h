fe839615a6bf510101b9cb80ac6cd99ae06231b7
Jerry Krinock <jerry@sheepsystems.com>
2017-04-28 05:30:20 -0700

    •  Added new class SSYSidebarController.

---
+++ SSYSidebarController.h
@@ -0,0 +1,134 @@
+#import <AppKit/AppKit.h>
+
+enum SSYSidebarControllerWhichEdge_enum {
+    SSYSidebarControllerWhichEdgeRight,
+    SSYSidebarControllerWhichEdgeLeft,
+    SSYSidebarControllerWhichEdgeBottom,
+    SSYSidebarControllerWhichEdgeTop
+};
+typedef enum SSYSidebarControllerWhichEdge_enum SSYSidebarControllerWhichEdge;
+
+/*!
+ @brief    SSYSidebarController toggles a *sidebar view* in a window between
+ completely collapsed and comletely expanded states, simultaneously adjusting,
+ as appropriate, the size and/or position of a sibling adjacent *main* view
+ and the window in which both views reside
+
+ @details
+
+ ## Auto Layout
+ 
+ SSYSidebarController does not use Auto Layout.  I have not thought much about
+ whether or not you could, for instance, use Auto Layout in the main and
+ sidebar views.  The use case I had for writing SSYSidebarController were
+ to replace NSDrawer instances in a old pre Auto-Layout project.
+ 
+ ## View Size Variability
+ 
+ Each time the sidebar is expanded, you may specify a minimum length (height
+ or width, depending on `whichEdge`) for the main view.  If this is less than
+ its current length, SSYSidebarView will partially collapse the main view, down
+ to that minimum, before expanding the window.
+ 
+ The sidebar length (height or width, depending on `whichEdge`) is fixed during
+ -awakeFromNib.  Attempting to change it may cause unexpected results.  Its
+ other dimention (width or height) may be changed.
+ 
+ ## Window Edges
+
+ The "sidebar" view can appear on any of the four window edges; in
+ other words it can also be a *topbar* or *bottombar*.  But we call it a
+ *sidebar* in all cases.  Think *side information* or *side controls*.  You may
+ theoretically put sidebars on all four edges of a window, although I've only
+ tested one and two.
+
+ ## Usage
+
+ Note that, unlike NSSplitViewController, SSYSidebarController does not inherit
+ from NSViewController.  It actually controls not one view but two – the *main*
+ and the *sidebar* views.  Typically, you instantiate one SSYSidebarController
+ in a xib or storyboard that contains the window, main view and sidebar view,
+ and the latter two should be connected to SSYSidebarController's outlets.
+ It is recommended to place the main view and sidebar view as siblings in a
+ window's content view, abutting one another and filling the content view in
+ the same way as they would when the sidebar is expanded.  Nonetheless, your
+ window will initially appear with the sidebar collapsed, because
+ SSYSidebarController collapses it during its -awakeFromNib.
+
+ You may also instantiate SSYSidebarController in code.
+
+ ## Relation to NSDrawer and NSSplitView
+
+ Since 2012 or so, NSDrawer has been dis-recommended by Apple.  When
+ a window with a NSDrawer loads, a bunch of crap is logged to the Xcode
+ console, and also, sometimes, drawer content does not load.  One solution is
+ to replace NSDrawer with NSSplitView and NSSplitViewController, but those
+ two are quite complicated to use, buggy.  If you have obvious buttons in
+ your user interface for opening and closing the drawer, maybe you don't need
+ the draggable devider bar.  In that case, you can simply put your drawer
+ contents in a sibling subview to the window's main view and instantiate a
+ SSYSidebarController in your window controller.
+
+ ## Troubleshooting
+
+ If your views are not sizing as expected, think about their autoresize masks
+ ("springs and struts" in Interface Builder), and those of their subviews.
+ */
+IB_DESIGNABLE
+@interface SSYSidebarController : NSObject
+
+@property (assign) IBOutlet NSView* mainView;
+@property (assign) IBOutlet NSView* sidebarView;
+
+/*!
+ @details  The values of this property must be one of the values of the enum
+ SSYSidebarControllerWhichEdge.  Unfortunately, as of Xcode 8.2, Interface
+ Builder does not know how to show a control for enum types, and thus will not
+ show a control for an enum typed property in the Attributes Inspector.  To
+ work around this limitation so that a control shows in Interface Builder, we
+ type this property as NSInteger.  Sigh.
+ */
+@property (assign) IBInspectable NSInteger whichEdge;
+
+/*!
+ @brief    Initializes a SSYSidebarController instance and collapses the given
+ sidebar view so that only the given main view is visible
+
+ @details  Typically, you instantiate SSYSidebarController in Interface Builder
+ and will not use this method.  If instead you instantiate SSYSidebarController
+ in code, you will call this method, possibly from your in your window
+ controller's -awakeFromNib implementation.
+
+ @param    mainView  The main part of the window, which should be visible when
+ the window initially opens
+ @param    sidebarView  The auxiliary, aka "sidebar" or "drawer" view, which is
+ initially collapsed.
+ */
+- (instancetype)initWithMainView:(NSView*)mainView
+                     sidebarView:(NSView*)sidebarView
+                       whichEdge:(SSYSidebarControllerWhichEdge)whichEdge
+NS_DESIGNATED_INITIALIZER;
+
+/*!
+ @brief    Expands or collapses the sidebar view of the receiver
+
+ @details  When expanding, borrows length from the main view as available and,
+ if that is not enough, makes the window bigger, and if would cause the window
+ to overflow the screen, moves the window enough in the opposite direction to
+ prevent that overflow.  When collapsing, restores to the main view whatever
+ length was borrowed in the previous expanding.  Does nothing if expandPort
+ is passed YES when the sidebar is already expanded, or NO when the sidebar is
+ already collapsed.
+
+ @param    expandSidebar  YES to expand the sidebar, NO to collapse
+ @param    mainViewMinimum  When expanding, the minimum length (width or
+ height) in the direction of the expanding, to which the main view may view
+ may be collapsed to accomodate the expansion of the sidebar view.  Ignored
+ when collapsing.
+ @param    animate  YES to animate the expanding or collapsing
+ */
+- (void)expandSidebar:(BOOL)expand
+      mainViewMinimum:(CGFloat)mainViewMinimum
+              animate:(BOOL)animate ;
+
+@end
---
+++ SSYSidebarController.m
@@ -0,0 +1,245 @@
+#import "SSYSidebarController.h"
+
+enum SSYSidebarControllerExpansionDirection_enum {
+    SSYSidebarControllerExpandsHorizontally,
+    SSYSidebarControllerExpandsVertically
+};
+typedef enum SSYSidebarControllerExpansionDirection_enum SSYSidebarControllerExpansionDirection;
+
+@interface SSYSidebarController()
+
+@property (assign) BOOL isSidebarShowing;
+@property (assign) CGFloat sidebarLength;
+@property (assign) CGFloat lengthBorrowedFromMainView;
+
+@end
+
+
+@implementation SSYSidebarController
+
+- (instancetype)initWithMainView:(NSView*)mainView
+                     sidebarView:(NSView*)sidebarView
+                       whichEdge:(SSYSidebarControllerWhichEdge)whichEdge {
+    self = [self init];
+
+    if (self) {
+        self.mainView = mainView;
+        self.sidebarView = sidebarView;
+        self.whichEdge = whichEdge;
+    }
+
+    return self ;
+}
+
+- (void)awakeFromNib {
+    /* Grab the original length of the sidebar for future reference */
+    self.sidebarLength = [self lengthOfFrame:self.sidebarView.frame];
+
+    /* Initially, we want the sidebar to be collapsed, and the main view to
+     be its orignal (current) length. */
+    [self regardlesslyExpandSidebar:NO
+                    mainViewMinimum:[self lengthOfFrame:self.mainView.frame]
+                            animate:NO];
+}
+
+- (BOOL)mainIsAtZeroPosition {
+    BOOL answer = NO;
+    switch (self.whichEdge) {
+        case SSYSidebarControllerWhichEdgeRight:
+        case SSYSidebarControllerWhichEdgeTop:
+            answer = YES;
+            break;
+        case SSYSidebarControllerWhichEdgeLeft:
+        case SSYSidebarControllerWhichEdgeBottom:
+            answer = NO;
+            break;
+    }
+
+    return answer;
+}
+
+- (SSYSidebarControllerExpansionDirection)direction {
+    if (
+        (self.whichEdge == SSYSidebarControllerWhichEdgeLeft)
+        || (self.whichEdge == SSYSidebarControllerWhichEdgeRight)) {
+        return SSYSidebarControllerExpandsHorizontally;
+    }
+    else {
+        return SSYSidebarControllerExpandsVertically;
+    }
+}
+
+- (CGFloat)lengthOfFrame:(NSRect)frame {
+    if ([self direction] == SSYSidebarControllerExpandsHorizontally) {
+        return frame.size.width;
+    } else {
+        return frame.size.height;
+    }
+}
+
+- (CGFloat)positionOfFrame:(NSRect)frame {
+    if ([self direction] == SSYSidebarControllerExpandsHorizontally) {
+        return frame.origin.x;
+    } else {
+        return frame.origin.y;
+    }
+}
+
+- (void)addLength:(CGFloat)length
+          toFrame:(NSRect*)frame_p {
+    if ([self direction] == SSYSidebarControllerExpandsHorizontally) {
+        (*frame_p).size.width += length;
+    } else {
+        (*frame_p).size.height += length;
+    }
+}
+
+- (void)addPosition:(CGFloat)position
+            toFrame:(NSRect*)frame_p {
+    if ([self direction] == SSYSidebarControllerExpandsHorizontally) {
+        (*frame_p).origin.x += position;
+    } else {
+        (*frame_p).origin.y += position;
+    }
+}
+
+- (void)setLength:(CGFloat)length
+          ofFrame:(NSRect*)frame_p {
+    if ([self direction] == SSYSidebarControllerExpandsHorizontally) {
+        (*frame_p).size.width = length;
+    } else {
+        (*frame_p).size.height = length;
+    }
+}
+
+- (void)setPosition:(CGFloat)position
+            ofFrame:(NSRect*)frame_p {
+    if ([self direction] == SSYSidebarControllerExpandsHorizontally) {
+        (*frame_p).origin.x = position;
+    } else {
+        (*frame_p).origin.y = position;
+    }
+}
+
+- (void)expandSidebar:(BOOL)expand
+      mainViewMinimum:(CGFloat)mainViewMinimum
+              animate:(BOOL)animate {
+    if (self.isSidebarShowing == expand) {
+        /* Sidebar is already expanded/collapsed as desired. */
+        return;
+    }
+    self.isSidebarShowing = expand;
+
+    [self regardlesslyExpandSidebar:expand
+                    mainViewMinimum:mainViewMinimum
+                            animate:animate];
+}
+
+- (void)regardlesslyExpandSidebar:(BOOL)expand
+                  mainViewMinimum:(CGFloat)mainViewMinimum
+                          animate:(BOOL)animate {
+    /* This method contains dual-purpose generalized code:
+     • We use 'length' to mean either 'height' or 'width'
+     • We use 'position' to mean either 'x' or 'y'
+     where the "either" choice depends on whether we are expanding and
+     collapsing horizontally or vertically, which in turn depends on the
+     property `whichEdge`. */
+
+    NSRect windowFrame = self.mainView.window.frame;
+    NSRect windowContentFrame = self.mainView.window.contentView.frame;
+    CGFloat delta;
+    if (expand) {
+        CGFloat availableLengthFromMainView ;
+        availableLengthFromMainView = [self lengthOfFrame:windowContentFrame] - mainViewMinimum;
+
+        delta = self.sidebarLength - availableLengthFromMainView;
+        if (delta > 0.0) {
+            [self addLength:delta
+                    toFrame:&windowFrame];
+            [self addLength:delta
+                    toFrame:&windowContentFrame];
+
+            NSRect useableFrame = self.mainView.window.screen.visibleFrame ;
+            // That useableFrame does not include main menu bar or Dock.
+            CGFloat screenLength = [self lengthOfFrame:useableFrame] ;
+            CGFloat overflowLength;
+            if ([self mainIsAtZeroPosition]) {
+                overflowLength = [self positionOfFrame:windowFrame] + [self lengthOfFrame:windowFrame] - screenLength;
+                if (overflowLength > 0.0) {
+                    [self addPosition:-overflowLength
+                              toFrame:&windowFrame];
+                }
+            } else {
+                overflowLength = -([self positionOfFrame:windowFrame] - delta);
+                if (overflowLength > 0.0) {
+                    [self addPosition:overflowLength
+                              toFrame:&windowFrame];
+                }
+            }
+        }
+
+        self.lengthBorrowedFromMainView = availableLengthFromMainView;
+
+        if (![self mainIsAtZeroPosition]) {
+            [self addPosition:-delta
+                      toFrame:&windowFrame];
+        }
+    } else {
+        delta = self.sidebarLength - self.lengthBorrowedFromMainView;
+        [self addLength:-delta
+                toFrame:&windowFrame];
+        [self addLength:-delta
+                toFrame:&windowContentFrame];
+        if (![self mainIsAtZeroPosition]) {
+            [self addPosition:delta
+                      toFrame:&windowFrame];
+        }
+    }
+
+    [self.mainView.window setFrame:windowFrame
+                           display:YES
+                           animate:YES];
+
+    NSRect mainFrame = self.mainView.frame;
+    NSRect sidebarFrame = self.sidebarView.frame;
+
+    [self setLength:(expand ? self.sidebarLength : 0.0)
+            ofFrame:&sidebarFrame];
+
+    CGFloat newLengthOfMainFrame = [self lengthOfFrame:windowContentFrame] - [self lengthOfFrame:sidebarFrame];
+    CGFloat newLengthOfSidebarFrame = (expand ? self.sidebarLength : 0.0);
+
+    [self setLength:newLengthOfMainFrame
+            ofFrame:&mainFrame];
+    [self setLength:newLengthOfSidebarFrame
+            ofFrame:&sidebarFrame];
+
+    if ([self mainIsAtZeroPosition]) {
+        [self setPosition:0.0
+                  ofFrame:&mainFrame];
+        [self setPosition:newLengthOfMainFrame
+                  ofFrame:&sidebarFrame];
+    } else {
+        [self setPosition:0.0
+                  ofFrame:&sidebarFrame];
+        [self setPosition:newLengthOfSidebarFrame
+                  ofFrame:&mainFrame];
+    }
+
+
+    if (animate) {
+        NSView* mainAnimator = [self.mainView animator];
+        NSView* sidebarAnimator = [self.sidebarView animator];
+        [mainAnimator setFrame:mainFrame];
+        [sidebarAnimator setFrame:sidebarFrame];
+    } else {
+        [self.mainView setFrame:mainFrame];
+        [self.sidebarView setFrame:sidebarFrame];
+    }
+
+    [self.mainView setNeedsDisplay: YES];
+    [self.sidebarView setNeedsDisplay:YES];
+}
+
+
+@end
